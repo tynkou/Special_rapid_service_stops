cedbd99a562f64e14672e0886a2b98f20744ff2e
tynkou <q7fhtcdgm5@privaterelay.appleid.com>
2026-09-21 10:16:52 +0900

    Update system.py

--- system.py
+++ system.py
@@ -1,10 +1,16 @@
 from flask import Flask,render_template,request,session,redirect,url_for
 app:Flask=Flask(__name__)
 stations=["上郡","有年","播州赤穂","坂越","西相生","相生","竜野","網干","はりま勝原","英賀保","手柄山平和公園","姫路",
-"加古川","西明石","明石","神戸","三ノ宮","芦屋","尼崎","大阪","新大阪","高槻","京都","山科","大津","石山","南草津","草津","守山","野洲","近江八幡","能登川","彦根","米原","坂田","田村","長浜","虎姫","河毛","高月","木ノ本","余呉","近江塩津","新疋田","敦賀","大津京","比叡山坂本","堅田","近江舞子","北小松","近江高島","安曇川","新旭","近江今津","近江中庄","マキノ","永原"]
+"加古川","西明石","明石","神戸","三ノ宮","芦屋","尼崎","大阪","新大阪","高槻","京都","山科","大津","石山","南草津",
+"草津","守山","野洲","近江八幡","能登川","彦根","米原","坂田","田村","長浜","虎姫","河毛","高月","木ノ本","余呉",
+"近江塩津","新疋田","敦賀","大津京","比叡山坂本","堅田","近江舞子","北小松","近江高島","安曇川","新旭","近江今津",
+"近江中庄","マキノ","永原"]
 app.secret_key="secret_key_here1234567890"
 #回答画面
-@app.route("/",methods=["GET","POST"])
+@app.route("/")
+def none():
+    return redirect(url_for("index"))
+@app.route("/index",methods=["GET","POST"])
 def index():
     if "answerd" not in session:
         session["answerd"]=[]
@@ -14,7 +20,6 @@ def index():
         return render_template("answer.html",result_q="")
     elif request.method=="POST":
         ans=request.form.get("answer")
-        print(ans)
         user_answerd = session["answerd"]
         if ans=="ギブアップ":
             return redirect(url_for("result"))
@@ -26,15 +31,23 @@ def index():
             result_q="回答済み"
         else:
             result_q="不正解"
-        print(result_q)
-        print(session["answerd"])
         ans=""
         return render_template("answer.html",result_q=result_q)
 
 @app.route("/result")
 def result():
-    score=len(session["answerd"])
-    session.clear()
-    return render_template("result.html",score=score)
+    try:
+        no_answerd=[]
+        for x in stations:
+            if x not in session["answerd"]:
+                no_answerd.append(x)
+        not_ans=""
+        for y in no_answerd:
+            not_ans+=y+"、"
+        score=len(session["answerd"])
+        session.clear()
+        return render_template("result.html",score=score,no_answerd=not_ans)
+    except KeyError:
+        return redirect(url_for("index"))
 if __name__ == "__main__":
     app.run(debug=True)
